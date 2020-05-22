85e79908bf261dba7fe470582cd7d2cbf73537ae
junstice <junstice@naver.com>
2020-05-22 17:36:05 +0900

    초기 설정으로 구축 + jsp 연동

---
+++ src/main/java/com/example/demo/DemoSpringApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringApplication.class, args);
+	}
+
+//	@RequestMapping("/")
+//	public String index() {
+//		return "HELLO WORLD!";
+//	}
+}
